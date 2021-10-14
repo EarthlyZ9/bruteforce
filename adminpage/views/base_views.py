c7cd761dc98662de5ff17f5b8ff47fabeaf024db
linda2927 <linda2927@naver.com>
2021-10-14 13:54:37 +0900

    bfadmin site

--- adminpage/views/base_views.py
+++ adminpage/views/base_views.py
@@ -51,7 +51,7 @@ def admin_page(request):
     for row in read_file:
       username = row[username]
       print(username)
-      points = row[points]
+      points = int(row[points])
       user = Progress.objects.filter(user__username=username).first()
       if period == '1':
         user.progress1 = points
