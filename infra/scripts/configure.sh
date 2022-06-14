3d1b5f23922683aa0e3af29d2cf8ad9f4e6d1f0d
linda2927 <linda2927@naver.com>
2022-06-14 18:15:26 +0900

    deploy configuration

--- infra/scripts/configure.sh
+++ infra/scripts/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/aws-test/infra/configs
+cd /home/ubuntu/bruteforce/infra/configs
 sudo cp nginx/nginx.conf /etc/nginx
 sudo cp -r nginx/conf.d /etc/nginx
 sudo cp systemd/gunicorn.socket /etc/systemd/system
