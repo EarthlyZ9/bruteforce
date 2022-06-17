45fda7f04f6759648ce324b8255089399b09be59
JS <linda2927@naver.com>
2022-06-17 17:17:20 +0900

    collect static files in S3

--- bruteforceuniv/bruteforceuniv/settings/base.py
+++ bruteforceuniv/bruteforceuniv/settings/base.py
@@ -169,6 +169,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # CSRF
 CSRF_TRUSTED_ORIGINS = ['https://www.bruteforceuniv.com', 'https://bruteforceuniv.com']
